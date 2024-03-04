0a4a466a63f01eacba8599d23205f6a171caf044
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-04 16:34:13 +0100

    s

--- ajout_music_veref.php
+++ ajout_music_veref.php
@@ -10,7 +10,7 @@
         if (!empty($_POST['titre'])) {
         require 'connect.php';
         try {
-            $name =$_FILES["fichier"]["name"];
+            $name =$_FILES[$_POST['fichier']]["name"];
             $db = new PDO(DNS, LOGIN, PASSWORD, $options);
             $sql = 'INSERT INTO RF_TRACK( name_track, filename_track, pathname_track) 
                     VALUES ( :nam, :fil,:fil)';
