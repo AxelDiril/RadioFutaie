03bd7f72b9303f62b6fda10d32a52e2af0d3a1e2
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-05 14:44:47 +0100

    uo

---
+++ sign_in.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang=fr>
+<head>
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+
+    <form action="login.php" method="POST">
+        <label for="idlogin">Login : </label>
+        <input type="text" id="idlogin" name="login"><br>
+        <label for="password">Mot de passe : </label>
+        <input type="password" id="idpassword" name="password">
+        <input type="submit">
+    </form>
+
+    <?php
+
+    ?>
+</body>
