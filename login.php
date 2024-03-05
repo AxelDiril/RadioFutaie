4edd139d6b985caf06b0761a8a51126a40f789e6
mathiasCarlier <mathiasc1811@gmail.com>
2024-03-05 10:35:30 +0100

    azefrgt

--- login.php
+++ login.php
@@ -2,25 +2,23 @@
 <html >
     <head>
 
-        <title>veref</title>
+        <title>login</title>
     </head>
     <body>
         <h1></h1><br><br>
         <?php
-        if (!empty($_POST['titre'])) {
+        if (!empty($_POST[''])) {
         require 'connect.php';
         try {
             $db = new PDO(DNS, LOGIN, PASSWORD, $options);
-            $sql = 'INSERT INTO RF_TRACK( name_track, filename_track, pathname_track) 
-                    VALUES ( :nam, :fil,:pass)';
+            $sql = '';
             $statement = $db->prepare($sql);
             $statement->bindParam('nam', $_POST['titre']);
             $statement->bindParam('fil', $_POST['fichier']);
-            $pas="music/".$_POST['fichier'];
-            $statement->bindParam('pass', $pas);
+            
             
             $statement->execute();
-
+            
             echo '<label>La musique ' . $_POST['titre'] . ' a bien été ajoutée</label><br><br>';
             echo "<a href='ajout_music.php'>Retour à l'index</a>";
             
@@ -30,7 +28,7 @@
             die('echec :' . $e->getMessage());
         }
         }else {
-            echo 'Le libellé doive être saisie !';
+            echo 'Les libellés doive être saisie !';
             echo "<a href='ajout_music.php'>Retour à l'insersion</a>";
         }
         ?>
