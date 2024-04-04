3b6a2beb9be4542b1acd144b7a853560d6991d9d
AxelDiril <156237319+AxelDiril@users.noreply.github.com>
2024-04-04 09:16:13 +0200

    Mise à jour de connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    define('DNS', 'mysql:host=localhost;port=3306;dbname=metallica');
-    define('LOGIN', 'metallica');
-    define ('PASSWORD', 'rae1eeYeenoh');
+    define(,);
+    define(,);
+    define (,);
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
                      PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
                      PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8");
