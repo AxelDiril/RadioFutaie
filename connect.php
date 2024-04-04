ffa43840a9279dda8b80e0265fb4514c1343f121
AxelDiril <156237319+AxelDiril@users.noreply.github.com>
2024-04-04 09:21:46 +0200

    Suppresion de connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define(,);
-    define(,);
-    define (,);
-    $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-                     PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
-                     PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8");
-?>
